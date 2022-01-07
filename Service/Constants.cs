ec9db444191817320fbe48a041bfe2620234a403
Abbos <95769114+KhaydarovAbbos@users.noreply.github.com>
2022-01-07 10:13:55 +0500

    Add files via upload

---
+++ Service/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalCenter.Service
+{
+    public class Constants
+    {
+
+        public static readonly string StudentsJsonPath = @"..\..\..\Database\Students.json";
+        public static readonly string GroupsJsonPath = @"..\..\..\Database\Groups.json";
+        public static readonly string AdminsJsonPath = @"..\..\..\Database\Admins.json";
+
+    }
+}
